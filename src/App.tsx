c324259db985c72c369dc0d99c6ccd6f149f95fc
Melissa Astbury <melissajastbury@gmail.com>
2020-09-09 10:45:48 +0100

    made a check answer function whereby the answer clicked by the user is checked again the correct answer to the question. If correct the score is increased

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,7 @@ import QuestionCard from "./components/QuestionCard/QuestionCard";
 
 const TOTAL_QUESTIONS = 10;
 
-type AnswerObject = {
+export type AnswerObject = {
   question: string;
   answer: string;
   correct: boolean;
@@ -19,13 +19,13 @@ const App = () => {
   const [number, setNumber] = useState(0);
   const [userAnswers, setUserAnswers] = useState<AnswerObject[]>([]);
   const [score, setScore] = useState(0);
-  const [gameOver, setGameover] = useState(true);
+  const [gameOver, setGameOver] = useState(true);
 
   console.log(questions);
 
   const startQuiz = async () => {
     setLoading(true);
-    setGameover(false);
+    setGameOver(false);
 
     const newQuestions = await fetchQuestions(TOTAL_QUESTIONS, Difficulty.EASY);
 
@@ -36,9 +36,34 @@ const App = () => {
     setLoading(false);
   };
 
-  const checkAnswer = (e: React.MouseEvent<HTMLButtonElement>) => {};
+  const checkAnswer = (e: React.MouseEvent<HTMLButtonElement>) => {
+    if (!gameOver) {
+      //user answer
+      const answer = e.currentTarget.value;
+      //check user answer to correct answer
+      const correct = questions[number].correct_answer === answer;
+      //add to score if correct
+      if (correct) setScore((prev) => prev + 1);
+      //save answer in user answer array in the State
+      const answerObject = {
+        question: questions[number].question,
+        answer,
+        correct,
+        correctAnswer: questions[number].correct_answer,
+      };
+      setUserAnswers((prev) => [...prev, answerObject]);
+    }
+  };
 
-  const nextQuestion = () => {};
+  const nextQuestion = () => {
+    //move onto next question
+    const nextQuestion = number + 1;
+    if (nextQuestion === TOTAL_QUESTIONS) {
+      setGameOver(true);
+    } else {
+      setNumber(nextQuestion);
+    }
+  };
 
   return (
     <div className="App">
